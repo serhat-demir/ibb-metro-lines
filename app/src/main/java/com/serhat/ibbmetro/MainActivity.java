9cf851d7f9bdfcdf5cad5c92368d96c1a71d703e
serhatdemirr <serhatdmr@protonmail.com>
2022-07-12 13:51:58 +0300

    implement view binding

--- app/src/main/java/com/serhat/ibbmetro/MainActivity.java
+++ app/src/main/java/com/serhat/ibbmetro/MainActivity.java
@@ -1,14 +1,32 @@
 package com.serhat.ibbmetro;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import com.serhat.ibbmetro.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+    private Context context;
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        context = MainActivity.this;
+
+        //toolbar
+        binding.toolbarLines.setTitle(getString(R.string.app_name));
+        binding.toolbarLines.setSubtitle(getString(R.string.istanbul_metro_lines));
+        setSupportActionBar(binding.toolbarLines);
+
+        //recyclerview
+        binding.recyclerViewLines.setHasFixedSize(true);
+        binding.recyclerViewLines.setLayoutManager(new LinearLayoutManager(context));
     }
 }
--- app/src/main/java/com/serhat/ibbmetro/StationsActivity.java
+++ app/src/main/java/com/serhat/ibbmetro/StationsActivity.java
@@ -1,14 +1,32 @@
 package com.serhat.ibbmetro;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import com.serhat.ibbmetro.databinding.ActivityStationsBinding;
+
 public class StationsActivity extends AppCompatActivity {
+    private Context context;
+    private ActivityStationsBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_stations);
+        binding = ActivityStationsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        context = StationsActivity.this;
+
+        //toolbar
+        binding.toolbarStations.setTitle(getString(R.string.app_name));
+        binding.toolbarStations.setSubtitle(getString(R.string.metro_stations));
+        setSupportActionBar(binding.toolbarStations);
+
+        //recyclerview
+        binding.recyclerViewStations.setHasFixedSize(true);
+        binding.recyclerViewStations.setLayoutManager(new LinearLayoutManager(context));
     }
 }
