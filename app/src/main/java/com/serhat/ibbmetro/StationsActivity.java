4e21f68350fb078c0835147c8f28a72f48cca345
serhatdemirr <serhatdmr@protonmail.com>
2022-07-12 13:44:43 +0300

    design stations activity, rename views in main activity

---
+++ app/src/main/java/com/serhat/ibbmetro/StationsActivity.java
@@ -0,0 +1,14 @@
+package com.serhat.ibbmetro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StationsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stations);
+    }
+}
